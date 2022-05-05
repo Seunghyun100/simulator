40cfaea7b69184d533b62b0aeca9503bdbdb2702
Seunghyun100 <87792403+Seunghyun100@users.noreply.github.com>
2022-05-05 13:29:17 +0900

    revise the struct name to upper case

--- configuration/circuit-configuration.jl
+++ configuration/circuit-configuration.jl
@@ -1,61 +1,85 @@
 module CircuitConfiguration
 
-    abstract type Error end
-    abstract type Circuit end
-
-    abstract type Operation <: Circuit end
-    abstract type CircuitQubit <: Circuit end
-
+    abstract type Operation end
     abstract type Gate <: Operation end
-    abstract type Initialization <: Operation end
-    abstract type Measure <: Operation end
 
-    struct error <: Error
-        dependOnHeat::Float64
-        errorRate::Float64
-    end
-
-    struct singleGate <: Gate
-        name::String
-        duration::Float64
-        error::error
-        qubit::CircuitQubit
-    end
-
-    struct multiGate <: Gate
-        name::String
-        duration::Float64
-        error::error
-        noOfQubits::Int64
-        qubits::Array{CircuitQubit}
-    end
-
-    struct initilization <: Initialization
-        duration::Float64
-        qubit::CircuitQubit
-        error::error
-    end
-
-    struct measure <: Measure
-        name::String
-        duration::Float64
-        qubit::CircuitQubit
-        error::error
-    end
-
-    struct circuitQubit <: CircuitQubit
+    mutable struct CircuitQubit
         id::Int64
         operations::Array{Operation}
     end
 
-    struct circuit <: Circuit
+    mutable struct Circuit
         noOfQubits::Int64
         qubits::Array{CircuitQubit}
     end
 
-    function configure()
+    """
+    This part is about operations.
+    """
+
+    struct Fidelity
+        errorRate::Float64
+        dependOnHeat::Float64 # TODO
+        function fidelity(errorRate::Float64=0.0, dependOnHeat::Float64=0.0)
+            @assert(0.0<=errorRate<=1.0,"Out of range of the error rate that between 0.0 to 1.0")
+            new(errorRate,dependOnHeat)
+        end
     end
 
-    function openConfigFile(filePath::String)
+    struct SingleGate <: Gate
+        name::String
+        duration::Float64
+        fidelity::Fidelity
+        function singleGate(name::String="None", duration::Float64=0.0, fidelity::Fidelity=fidelity())
+            @assert(name=="None", "You must name the single gate.")
+            if duration ==0.0
+                @info "The duration of operation is 0"
+            end
+            new(name, duration, fidelity)
+        end
+    end
+
+    struct MultiGate <: Gate
+        name::String
+        duration::Float64
+        fidelity::Fidelity
+        noOfQubits::Int64
+        qubits::Array{CircuitQubit}
+    end
+
+    struct Initilization <: Operation
+        duration::Float64
+        fidelity::Fidelity
+    end
+
+    struct Measure <: Operation
+        name::String
+        duration::Float64
+        fidelity::Fidelity
+    end
+
+    function configure(configType::String, specification::Tuple{Vararg{Tuple{String, Any}}})::Tuple
+        config = nothing
+        ex = "$configType("
+        for i in specification
+            ex = ex * i[2]*','
+        end
+        ex = ex * ')'
+        config = eval(ex)
+        return (configType, config)
+    end
+
+    function openConfigFile(filePath::String)::Dict
+        configuration = Dict()
+        
+        # TODO: parsing the configuration file
+        while true
+            configType = nothing
+            specification = nothing
+            config = configure(configType, specification)
+            configuration[config[1]] = config[2]
+        end
+
+        return configuration
     end
 end
