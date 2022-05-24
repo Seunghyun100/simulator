13736cb7c4fafe483adae274d63c3a910cbc6370
Seunghyun100 <87792403+Seunghyun100@users.noreply.github.com>
2022-05-24 23:19:20 +0900

    build circuit maker

--- circuit_maker.jl
+++ circuit_maker.jl
@@ -1,6 +1,5 @@
 import JSON
 
-
 """
 Input
 """
@@ -8,14 +7,53 @@ name = # name
 noQubits = #number of qubits
 
 
+"""
+Operation
+"""
+x = "x"
+y = "y"
+z = "z"
+rx = "rx"
+ry = "ry"
+rz = "rz"
+h = "h"
+s = "s"
+t = "t"
+    
+cx = "cx"
+cy = "cy"
+cz = "cz"
+rxx = "rxx"
+ryy = "ryy"
+rzz = "rzz"
+swap = "swap"
+toffoli = "toffoli"
+    
+init = "init"
+m = "commonMeasure"
+fast_m = "snspMeasure"
+
+"""
+function
+"""
 
 id = 1
 
-function two(name, q1, q2)
+# two-qubit gate
+function 2(name, q1, q2)
     output = [id,name,q1,q2]
     id += 1
+    push!(circuit["q$q1"], output)
+    push!(circuit["q$q2"], output)
     return output
 end
+
+#single gate
+function 1(name, q)
+    push!(circuit["q$q"], name)
+    return
+end
+#measurement
 
 function initCircuit(no::Int64)
     circuit = Dict()
@@ -26,6 +64,11 @@ function initCircuit(no::Int64)
 end
 
 circuit = initCircuit(noQubits)
+
+"""
+Circuit
+"""
+
 
 
 """
